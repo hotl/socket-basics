7d9968286bd3ed7686399e37962f4a8cfb0f8ff9
Timothy Ho <timothyho0820@gmail.com>
2017-05-03 20:58:33 -0700

    Adding client-server listeners

--- server.js
+++ server.js
@@ -11,8 +11,17 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 // Event listener
-io.on('connection', function() {
+io.on('connection', function(socket) {
 	console.log('User connected via socket.io');
+
+	socket.on('message', function(message) {
+		console.log('Message received:', message.text);
+		socket.broadcast.emit('message', message); // sends it to all connections, omitting the sending connection
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application'
+	}); //1st arg is name of event, 2nd is data
 });
 
 http.listen(PORT, function() {
