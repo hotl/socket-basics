b9bf691dbbad35e572a0af6e66551560f9769525
Timothy Ho <timothyho0820@gmail.com>
2017-05-22 21:32:34 -0700

    Add @currentUser functionality

--- server.js
+++ server.js
@@ -7,11 +7,44 @@ const app = express();
 // Server will listen to
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const clientInfo = {};
 
 // allows us to serve static files
 app.use(express.static(__dirname + '/public'));
 
-let clientInfo = {};
+// Sends current users to provided socket
+function sendCurrentUsers(socket) {
+	let info = clientInfo[socket.id];
+	let users = [];
+
+	if (typeof info === undefined) return;
+	Object.keys(clientInfo).forEach((socketId) => {
+		if (clientInfo[socketId].room === info.room) {
+			users.push(clientInfo[socketId].name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current Users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	})
+}
+
+// Sends private message to user
+function sendPrivateMessage(socket, user) { 
+	Object.keys(clientInfo).forEach((socketId) => {
+		if (clientInfo[socketId].name === user.name) {
+			user.userId = socketId;
+		}
+	});
+	socket.join(user.userId);
+	socket.broadcast.to(user.userId).emit('message', {
+		text: 'Private message',
+		timestamp: moment().valueOf(),
+		name: clientInfo[socket.id].name
+	});
+}
 
 // Event listener
 io.on('connection', function(socket) {
@@ -28,16 +61,24 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('message', function(message) {
-		console.log('Message received:', JSON.stringify(message));
+		console.log('Message received:', JSON.stringify(message, null, 2));
+
+		if (message.text === '@currentUsers') return sendCurrentUsers(socket);
+		else if (message.text.substr(0, 8) === '@private') {
+			let user = {
+				name: message.text.substr(8).trim()
+			};
+			return sendPrivateMessage(socket, user);
+		}
 		message.timestamp = moment().valueOf();
 		io.to(clientInfo[socket.id].room).emit('message', message);
-		console.log(JSON.stringify(clientInfo, null, 3));
 	});
 
 	socket.on('disconnect', function() {
 		let userData = clientInfo[socket.id];
 		if (typeof userData !== undefined) {
 			socket.leave(userData.room);
+			delete clientInfo[socket.id];
 			io.to(userData.room).emit('message', {
 				text: userData.name + ' has disconnected',
 				timestamp: moment().valueOf(),
